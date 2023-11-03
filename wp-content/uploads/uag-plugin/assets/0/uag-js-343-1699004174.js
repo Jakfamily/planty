ef11f31a84a22e1d55847eee563dbb46f771ad53
jakfamily <fariajeanbap@gmail.com>
2023-11-03 13:36:00 +0100

    realisation de la page nous rencontrer

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-343-1699004174.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBForms.init( {"block_id":"01f07dc0","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"dev-email@wpengine.local","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Soumission du formulaire","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Veuillez remplir le captcha ci-dessus.","confirmationUrl":""}, '.uagb-block-01f07dc0', 343 );
+});
+ });
