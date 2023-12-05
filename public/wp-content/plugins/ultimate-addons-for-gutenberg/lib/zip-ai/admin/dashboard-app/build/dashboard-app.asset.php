c5688b335aafc9b75a0a401513a4fe3e8d042c6c
jakfamily <fariajeanbap@gmail.com>
2023-12-05 10:53:14 +0100

    modification formulaire afin de mettre les input dans le form

--- public/wp-content/plugins/ultimate-addons-for-gutenberg/lib/zip-ai/admin/dashboard-app/build/dashboard-app.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'e0b542f13a73b6cf6e6f');
---
+++ wp-content/plugins/ultimate-addons-for-gutenberg/admin-core/assets/build/dashboard-app.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-hooks', 'wp-i18n'), 'version' => '7fbc6330cbc5bcdc9456');
